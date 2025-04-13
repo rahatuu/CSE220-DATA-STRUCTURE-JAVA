da3fdf2a06b1aff78f3493c5e7d23ec4ad5f7a6d
MD. RAHATUL ISLAM RAHAT <rahatulislam476@gmail.com>
2025-04-14 00:43:37 +0600

    Add files via upload

---
+++ LAB-1 Solve/Task5.java
@@ -0,0 +1,19 @@
+public class Task5 {
+    public static int sumDist(Node head, Integer[] distArr) {
+        // To Do
+        return 0; // Remove this line.
+    }
+
+    public static void main(String[] args) {
+        System.out.println("=========Test Case 1=============");
+        Node head1 = LinkedList.createList(new Integer[]{10, 16, -5, 9, 3, 4});
+        Integer[] dist = new Integer[] {2, 0, 5, 2, 8};
+	System.out.print("Given LinkedList: ");
+	LinkedList.printLL(head1);
+	System.out.print("Distance Array: ");
+	Arr.print(dist);
+        System.out.println("\nExpected Output: 4");
+	int returnedValue = Task5.sumDist(head1, dist);
+	System.out.println("Your Output: "+returnedValue); // This should print: Sum of Nodes: 4
+    }
+}
