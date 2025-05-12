569d1d6a2c8f15dcd594967f7a4c79ed8054ca0a
MD. RAHATUL ISLAM RAHAT <rahatulislam476@gmail.com>
2025-05-12 20:12:16 +0600

    Add files via upload

---
+++ LAB-4 Solve/Hashtable/Task2/HashTable.java
@@ -0,0 +1,58 @@
+/*You will have to complete the FruitNode Contrustor first
+ then within this class you only have to complete two methods
+ hashFunction() and insert()
+ the rest of the metods are already written
+ DO NOT TOUCH any other methods or codes*/
+public class HashTable {
+
+    //ht[] :: is the HashTable array that stores the FruitNode objects
+    private FruitNode[] ht;
+
+    //Constructor that initializes the HashTable array
+ //DO NOT change this Constructor
+    public HashTable(int size){
+        this.ht = new FruitNode[size];
+    }
+    
+    //This method basically prints the HashTable
+    //DO NOT change this method
+    public void show(){
+        for(int i=0; i<ht.length; i++){
+            System.out.print( i+" " );
+            FruitNode n = ht[i];
+            while (n!=null){
+                System.out.print("('"+n.fruit[0]+"', "+n.fruit[1]+") --> ");
+                n = n.next;
+            }
+            System.out.println();
+        }
+    }
+
+    //you need to COMPLETE this method
+    private int hashFunction( String key ){
+      int asciiconcat=0;
+      if(key.length()%2!=0){
+        for(int i=1;i<key.length();i+=2){
+          asciiconcat+= (int)key.charAt(i);}}
+      else{for(int i=0;i<key.length();i+=2){
+          asciiconcat+= (int)key.charAt(i);}}
+      return asciiconcat%ht.length;}
+
+    //you need to COMPLETE this method
+    //The insert() method will create a FruitNode using name(Key) & price(value)
+ //then inserts it in the proper hashed index
+    //If collision occurs resolve using the steps explained in the question
+    public void insert(String key, Integer value){
+            int hashedIndex = this.hashFunction( key );
+            FruitNode newPNode = new FruitNode( key, value );
+            if ( this.ht[hashedIndex] == null ){
+              this.ht[hashedIndex] = newPNode;}
+            else {
+              if((int)this.ht[hashedIndex].fruit[1]<(int)newPNode.fruit[1]){
+                newPNode.next = this.ht[hashedIndex];
+                this.ht[hashedIndex] = newPNode;}
+              else{FruitNode current = this.ht[hashedIndex];
+                for(int i=0;current.next!=null;i++){
+                  current=current.next;}
+                  current.next = newPNode;}}}  
+}
