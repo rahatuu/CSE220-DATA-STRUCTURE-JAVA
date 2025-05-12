e140fe91e697bc683b50a4ef1099f3c02b6f9fd0
MD. RAHATUL ISLAM RAHAT <rahatulislam476@gmail.com>
2025-05-12 20:10:26 +0600

    Add files via upload

---
+++ LAB-4 Solve/Hashtable/Task1/PairNode.java
@@ -0,0 +1,12 @@
+public class PairNode {
+    
+    String key;
+    Integer value;
+    PairNode next;
+
+    public PairNode( String k, Integer v ){
+        this.key=k;
+        this.value=v;
+    }
+    
+}
