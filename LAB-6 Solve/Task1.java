092d40cd3f5ad8a37e923bc01c495239c98e95db
MD. RAHATUL ISLAM RAHAT <rahatulislam476@gmail.com>
2025-06-01 22:01:57 +0600

    Update Task1.java

--- LAB-6 Solve/Task1.java
+++ LAB-6 Solve/Task1.java
@@ -4,10 +4,15 @@ public class Task1 {
     //======================TASK#1======================
     // This method takes only 1 parameter which is root
     // You'll need to create a new Mirrored Tree and return the new root
-    public static BTNode convertMirror( BTNode root ){
-        // TO DO
-        return null; //remove this line
-    }
+    public static BTNode convertMirror(BTNode root ){
+    if (root == null){ 
+        return null;}
+    else{
+       BTNode lefthand = convertMirror(root.left);
+       BTNode righthand = convertMirror(root.right);
+       root.left = righthand;
+       root.right = lefthand;
+       return root; }}
     //==================================================
 
 }
