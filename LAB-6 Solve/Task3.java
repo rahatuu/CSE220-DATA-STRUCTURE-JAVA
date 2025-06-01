2d23db0508133afbc713ce21ffe6d1a75eb7d7c0
MD. RAHATUL ISLAM RAHAT <rahatulislam476@gmail.com>
2025-06-01 22:09:07 +0600

    Update Task3.java

--- LAB-6 Solve/Task3.java
+++ LAB-6 Solve/Task3.java
@@ -6,10 +6,18 @@ public class Task3 {
         // This method returns an Integer
         // At times you may need to typeCast root.elem to Integer
         // You can use extra helper private static methods as per need
-        public static Integer sumTree( BTNode root ){
-            //TO DO
-            return null; //remove this line
-        }
+        public static Integer sumTree( BTNode root ){    
+          return sumTreeHelper(root, 0);}
+         private static Integer sumTreeHelper(BTNode ro,int lvl){
+          if(ro==null){return 0;} 
+          int sum=0;
+          if(lvl==0){ 
+           sum +=(Integer)ro.elem;}
+          else{
+           sum +=(Integer)ro.elem%lvl;}
+          sum+=sumTreeHelper(ro.left,lvl+1);
+          sum+=sumTreeHelper(ro.right,lvl+1);                
+          return sum;}
         //============================================================================
 
 }
